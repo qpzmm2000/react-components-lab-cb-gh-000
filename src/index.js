a9f569fdc07deabfabc971106fef386116758d4d
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-10-03 03:55:15 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -8,9 +8,40 @@ const BUTCHER_PRODUCTS = [
 ];
 
 // Define these exported classes
-export class OlderCoaster extends React.Component {};
-export class InFrontOfYou extends React.Component {};
-export class ButcherShop extends React.Component {};
+export class OlderCoaster extends React.Component {
+  render(){
+    return(<div class="oldercoaster">
+  <p>Two grannies having the time of their life!</p>
+  <p>Passengers:</p>
+  <ul>
+    <li>Agnes</li>
+    <li>Muriel</li>
+  </ul>
+</div>)
+  }
+};
+export class InFrontOfYou extends React.Component {
+  render(){
+    return(
+      <div>
+  <p>You shouldn''t look too far.</p>
+  <p>Sometimes, the solution is right in front of you.</p>
+</div>
+    )
+  }
+};
+export class ButcherShop extends React.Component {
+  render(){
+    return(
+      <div class="butcher-shop">
+  <p>Hello! We have the following products for sale today:</p>
+  <ul>
+    {BUTCHER_PRODUCTS.map(product => return(<li>product</li>))}
+  </ul>
+</div>
+    )
+  }
+};
 
 ReactDOM.render(
   React.createElement('div', {}, [
